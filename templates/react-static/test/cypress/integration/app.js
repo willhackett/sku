ee5e48b4f0a0a8b49eae75c1639f87d62d2afeaa
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2017-05-18 16:21:45 +1000

    WIP Fix Cypress test in static template

--- templates/react-static/test/cypress/integration/app.js
+++ templates/react-static/test/cypress/integration/app.js
@@ -2,6 +2,6 @@ describe('App Test', () => {
   beforeEach(() => cy.visit(Cypress.env('url')));
 
   it('should say hello world', () => {
-    cy.get('[data-automation=hello]').should('have.html', 'Hello world!');
+    cy.get('[data-automation=hello]').should('have.text', 'Hello world!');
   });
 });
