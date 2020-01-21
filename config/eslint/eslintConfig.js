3033ffd05e1ff9992c96cfa771b9c5c8a39d44c1
Ben <bjervis@seek.com.au>
2020-01-21 15:18:24 +1100

    Add new eslint rules

--- config/eslint/eslintConfig.js
+++ config/eslint/eslintConfig.js
@@ -1,7 +1,46 @@
 const { eslintDecorator } = require('../../context');
 
-const baseConfig = {
+const ERROR = 2;
+
+module.exports.baseConfig = {
   extends: require.resolve('eslint-config-seek'),
+  overrides: [
+    {
+      // TypeScript config
+      files: ['**/*.ts', '**/*.tsx'],
+      extends: [
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      ],
+      rules: {
+        '@typescript-eslint/no-floating-promises': ERROR,
+      },
+    },
+  ],
+  rules: {
+    'import/order': [
+      ERROR,
+      {
+        alphabetize: {
+          order: 'asc',
+        },
+        'newlines-between': 'always',
+        pathGroups: [
+          {
+            pattern: 'src/**',
+            group: 'external',
+            position: 'after',
+          },
+        ],
+        pathGroupsExcludedImportTypes: [
+          {
+            pattern: 'src/**',
+            group: 'external',
+            position: 'after',
+          },
+        ],
+      },
+    ],
+  },
 };
 
 module.exports = eslintDecorator(baseConfig);
